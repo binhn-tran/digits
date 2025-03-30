60ce17fd80a7484bf054cec73d27930c35814b0c
Binh Tran <binhtran@hawaii.edu>
2025-03-29 23:01:28 -1000

    list contacts page mockup finished in 20 minutes

---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { Card, Image } from 'react-bootstrap';
+
+// Define the Contact type
+type Contact = {
+  image: string;
+  firstName: string;
+  lastName: string;
+  address: string;
+  description: string;
+};
+
+/* Renders a single contact. See list/page.tsx. */
+const ContactCard = ({ contact }: { contact: Contact }) => (
+  <Card className="h-100">
+    <Card.Header>
+      <Image src={contact.image} width={75} />
+      <Card.Title>
+        {contact.firstName}
+        &nbsp;
+        {contact.lastName}
+      </Card.Title>
+      <Card.Subtitle>
+        {contact.address}
+      </Card.Subtitle>
+    </Card.Header>
+    <Card.Body>
+      <Card.Text>
+        {contact.description}
+      </Card.Text>
+    </Card.Body>
+  </Card>
+);
+
+export default ContactCard;
