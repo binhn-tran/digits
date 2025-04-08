9691b33033bf31e951664a517fec802f268cc450
Binh Tran <binhtran@hawaii.edu>
2025-04-07 22:23:12 -1000

    Update ContactCardAdmin.tsx

--- src/components/ContactCardAdmin.tsx
+++ src/components/ContactCardAdmin.tsx
@@ -5,7 +5,7 @@ import { Card, Image, ListGroup } from 'react-bootstrap';
 import NoteItem from '@/components/NoteItem';
 
 /* Renders a single contact. See list/page.tsx. */
-const ContactCardAdmin = ({ contact, notes }: { contact: Contact, notes: Note[] }) => (
+const ContactCardAdmin = ({ contact, notes = [] }: { contact: Contact, notes?: Note[] }) => (
   <Card className="h-100">
     <Card.Header>
       <Image src={contact.image} width={75} />
@@ -19,11 +19,15 @@ const ContactCardAdmin = ({ contact, notes }: { contact: Contact, notes: Note[] 
     <Card.Body>
       <Card.Text>{contact.description}</Card.Text>
       <ListGroup variant="flush">
-        {notes.map((note) => <NoteItem key={note.id} note={note} />)}
+        {Array.isArray(notes) && notes.map((note) => <NoteItem key={note.id} note={note} />)}
       </ListGroup>
       <p className="blockquote-footer">{contact.owner}</p>
     </Card.Body>
   </Card>
 );
+
+ContactCardAdmin.defaultProps = {
+  notes: [],
+};
 
 export default ContactCardAdmin;
